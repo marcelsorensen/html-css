4ae9ea92b5738362524f89686dbe382be1400097
Marcel <marcel.sorensen@teliacompany.com>
2024-03-14 22:39:05 +0100

    WIP, fetched pet data

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ async function start() {
 }
 
 start();
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsData = await petsPromise.json();
+  console.log(petsData);
+  petsData.forEach((pet) => {
+    console.log(pet.species);
+  });
+}
+
+petsArea();
